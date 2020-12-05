a24991d4df28ded15fa8fb95e16e1e9306321750
Josue Mendez <josuejoelmm@gmail.com>
2020-12-05 02:19:40 -0300

    set grid of dashboard

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,14 @@
 import Weather from './pages/home/Weather';
 import { GlobalProvider } from './state/Contex';
+import Layout from './components/Layout';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
         <GlobalProvider>
-            <Weather />
+            <Layout>
+                <Weather />
+            </Layout>
         </GlobalProvider>
     );
 }
--- src/app/pages/home/Weather/Weather.js
+++ src/app/pages/home/Weather/Weather.js
@@ -1,6 +1,12 @@
 import React, { useState, useContext } from 'react';
 import { GlobalContext } from '../../../state/Contex';
 import Loader from '../../../components/Loader';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
 
 export default function Weather() {
     const globalContext = useContext(GlobalContext);
@@ -29,10 +35,40 @@ export default function Weather() {
     }
 
     return (
-        <div>
-            <h1>asd App</h1>
-            <button onClick={startLoanding}>CLICK</button>
-            <Loader />
-        </div>
+        <Container className="py-4">
+            <Row>
+                <Col>
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>Search City</Form.Label>
+                            <Form.Control type="text" placeholder="New York" />
+                        </Form.Group>
+                    </Form>
+                </Col>
+            </Row>
+            <Row>
+                <Col sm>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img style={{width: '40%', margin: '0 auto'}} variant="top" src="https://www.placecage.com/60/40" />
+                    <Card.Body>
+                        <Card.Title>18*</Card.Title>
+                        <Card.Text>
+                        Con probabilidad de precipitacion: 50%
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+                </Col>
+                <Col sm>COL 2</Col>
+            </Row>
+            <Row>
+                <Col sm>1</Col>
+                <Col sm>2</Col>
+                <Col sm>3</Col>
+                <Col sm>4</Col>
+                <Col sm>5</Col>
+                <Col sm>6</Col>
+            </Row>
+        </Container>
     )
 }
