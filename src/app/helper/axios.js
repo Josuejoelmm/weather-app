ee0570de9b063c9dbc36a811bb7d382295d25fd8
Josue Mendez <josuejoelmm@gmail.com>
2020-12-07 16:49:26 -0300

    cleaning code

--- src/app/helper/axios.js
+++ src/app/helper/axios.js
@@ -5,6 +5,6 @@ const BASE_URL = 'http://api.openweathermap.org/data/2.5';
 export default axios.create({
     baseURL: BASE_URL,
     params: {
-        API_KEY: 'd3f7c9815cbbef3b7209c909c6b2d1f9'
+        API_KEY: 'YOUR API KEY HERE'
     }
 });
